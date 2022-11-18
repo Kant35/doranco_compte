2cbe76367720361d1881b0d1f84ffbc84034f8ba
QuentinVilfeu <quentin.vilfeu.dev@gmail.com>
2022-11-18 16:51:23 +0100

    Finalisation de l'ajout

---
+++ inc/functions.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * Fonction DEV qui permet un affichage clair des données (array, string, number ...)
+ */
+function debug($value){
+    echo "<pre>";
+        print_r($value);
+    echo "</pre>";
+}
